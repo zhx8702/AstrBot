dac840a887834512202fffe2d3c7e306a5de3657
Soulter <905617992@qq.com>
2025-05-11 20:08:14 +0800

    📦release: v3.5.9

--- astrbot/core/config/default.py
+++ astrbot/core/config/default.py
@@ -5,7 +5,7 @@
 import os
 from astrbot.core.utils.astrbot_path import get_astrbot_data_path
 
-VERSION = "3.5.8"
+VERSION = "3.5.9"
 DB_PATH = os.path.join(get_astrbot_data_path(), "data_v3.db")
 
 # 默认配置
