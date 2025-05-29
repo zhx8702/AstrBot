855858c236093a427006103c99560d20b0c52946
kwicxy <ones_republic@foxmail.com>
2025-05-29 09:31:15 +0800

    fix: Changed default theme to `PurpleTheme`

--- dashboard/src/plugins/vuetify.ts
+++ dashboard/src/plugins/vuetify.ts
@@ -10,7 +10,7 @@ export default createVuetify({
   directives,
 
   theme: {
-    defaultTheme: 'PurpleThemeDark',
+    defaultTheme: 'PurpleTheme',
     themes: {
       PurpleTheme,
       PurpleThemeDark
